ad72c1c3939cd8316e514928bd4f492df876bcae
M Farkas-Dyck <strake888@gmail.com>
2017-12-10 01:38:41 -0800

    fetch latest from Hackage

---
+++ Control/Monad/List.hs
@@ -0,0 +1,95 @@
+{-# OPTIONS -fallow-undecidable-instances #-}
+-- Needed for the same reasons as in Reader, State etc
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Monad.List
+-- Copyright   :  (c) Andy Gill 2001,
+--                (c) Oregon Graduate Institute of Science and Technology, 2001
+-- License     :  BSD-style (see the file libraries/base/LICENSE)
+--
+-- Maintainer  :  libraries@haskell.org
+-- Stability   :  experimental
+-- Portability :  non-portable (multi-parameter type classes)
+--
+-- The List monad.
+--
+-----------------------------------------------------------------------------
+
+module Control.Monad.List (
+    ListT(..),
+    mapListT,
+    module Control.Monad,
+    module Control.Monad.Trans,
+  ) where
+
+import Control.Monad
+import Control.Monad.Cont.Class
+import Control.Monad.Error.Class
+import Control.Monad.Reader.Class
+import Control.Monad.State.Class
+import Control.Monad.Trans
+
+-- ---------------------------------------------------------------------------
+-- Our parameterizable list monad, with an inner monad
+
+newtype ListT m a = ListT { runListT :: m [a] }
+
+mapListT :: (m [a] -> n [b]) -> ListT m a -> ListT n b
+mapListT f m = ListT $ f (runListT m)
+
+instance (Monad m) => Functor (ListT m) where
+    fmap f m = ListT $ do
+        a <- runListT m
+        return (map f a)
+
+instance (Monad m) => Monad (ListT m) where
+    return a = ListT $ return [a]
+    m >>= k  = ListT $ do
+        a <- runListT m
+        b <- mapM (runListT . k) a
+        return (concat b)
+    fail _ = ListT $ return []
+
+instance (Monad m) => MonadPlus (ListT m) where
+    mzero       = ListT $ return []
+    m `mplus` n = ListT $ do
+        a <- runListT m
+        b <- runListT n
+        return (a ++ b)
+
+-- ---------------------------------------------------------------------------
+-- Instances for other mtl transformers
+
+instance MonadTrans ListT where
+    lift m = ListT $ do
+        a <- m
+        return [a]
+
+instance (MonadIO m) => MonadIO (ListT m) where
+    liftIO = lift . liftIO
+
+instance (MonadCont m) => MonadCont (ListT m) where
+    callCC f = ListT $
+        callCC $ \c ->
+        runListT (f (\a -> ListT $ c [a]))
+
+type instance ErrorType (ListT m) = ErrorType m
+
+instance (MonadError m) => MonadError (ListT m) where
+    throwError       = lift . throwError
+    m `catchError` h = ListT $ runListT m
+        `catchError` \e -> runListT (h e)
+
+type instance EnvType (ListT m) = EnvType m
+
+instance (MonadReader m) => MonadReader (ListT m) where
+    ask       = lift ask
+    local f m = ListT $ local f (runListT m)
+
+type instance StateType (ListT m) = StateType m
+
+instance (MonadState m) => MonadState (ListT m) where
+    get = lift get
+    put = lift . put
+
---
+++ Control/Monad/Writer/Class.hs
@@ -0,0 +1,62 @@
+{-# LANGUAGE TypeFamilies, FlexibleContexts #-}
+{-# OPTIONS -fallow-undecidable-instances #-}
+-- Search for -fallow-undecidable-instances to see why this is needed
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Control.Monad.Writer.Class
+-- Copyright   :  (c) Andy Gill 2001,
+--                (c) Oregon Graduate Institute of Science and Technology, 2001
+-- License     :  BSD-style (see the file libraries/base/LICENSE)
+--
+-- Maintainer  :  libraries@haskell.org
+-- Stability   :  experimental
+-- Portability :  non-portable (multi-param classes, functional dependencies)
+--
+-- The MonadWriter class.
+--
+--      Inspired by the paper
+--      /Functional Programming with Overloading and
+--          Higher-Order Polymorphism/,
+--        Mark P Jones (<http://web.cecs.pdx.edu/~mpj/pubs/springschool.html>)
+--          Advanced School of Functional Programming, 1995.
+-----------------------------------------------------------------------------
+
+module Control.Monad.Writer.Class (
+    MonadWriter(..),
+    listens,
+    censor,
+    WriterType
+  ) where
+
+import Data.Monoid
+
+type family WriterType (m :: * -> *)
+
+-- ---------------------------------------------------------------------------
+-- MonadWriter class
+--
+-- tell is like tell on the MUD's it shouts to monad
+-- what you want to be heard. The monad carries this 'packet'
+-- upwards, merging it if needed (hence the Monoid requirement)}
+--
+-- listen listens to a monad acting, and returns what the monad "said".
+--
+-- pass lets you provide a writer transformer which changes internals of
+-- the written object.
+
+class (Monoid (WriterType m), Monad m) => MonadWriter m where
+    tell   :: WriterType m -> m ()
+    listen :: m a -> m (a, WriterType m)
+    pass   :: m (a, WriterType m -> WriterType m) -> m a
+
+listens :: (MonadWriter m) => (WriterType m -> b) -> m a -> m (a, b)
+listens f m = do
+    ~(a, w) <- listen m
+    return (a, f w)
+
+censor :: (MonadWriter m) => (WriterType m -> WriterType m) -> m a -> m a
+censor f m = pass $ do
+    a <- m
+    return (a, f)
+
